00cc66d98d099b3920b9b14a4643af525440de40
saranshg20 <saranshg20@iitk.ac.in>
2024-02-26 17:44:10 +0530

    Update CardLg component styling and add link to GitHub repository

--- pom-dependecy-parser-frontend/src/components/CardLg.jsx
+++ pom-dependecy-parser-frontend/src/components/CardLg.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function CardLg(props) {
     return (
-        <div className="bg-blue-100 p-6 m-4 rounded-md shadow-lg w-4/5">
+        <div className="bg-blue-200 hover:bg-blue-100 p-6 m-4 rounded-md shadow-lg w-4/5">
             <div className="flex justify-between">
-                <div className="text-xl font-light">{props.RepoName}</div>
-                <div className="flex">
+                <div className="text-xl w-1/2 font-light overflow-hidden overflow-ellipsis whitespace-nowrap">
+                    {props.RepoName}
+                </div>
+                <div className="flex justify-normal">
                     <div onClick={() => props.getDependencies(props.RepoId)}>
                         <img
                             className="mr-6 cursor-pointer"
@@ -17,6 +19,7 @@ function CardLg(props) {
                     </div>
                     <a href={props.RepoURL} target="_blank">
                         <img
+                            className="cursor-pointer"
                             width="30"
                             height="30"
                             src="https://img.icons8.com/ios-filled/50/github.png"
